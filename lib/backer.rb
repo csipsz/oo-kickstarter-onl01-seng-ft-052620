842027d0af982958ef39361732c42f50f55b7b6c
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-10 14:49:30 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -14,4 +14,16 @@ end
 
 
 
+class Project
+  attr_accessor :title, :backers
 
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
+end
